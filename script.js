ea78d1d587ccdd9165ba87203d34279d26010e3a
jubra4 <107110258+jubra4@users.noreply.github.com>
2023-06-08 18:49:11 +0200

    change name two words

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-let amountElement = document.querySelector(".js-price");
-let currencyElement = document.querySelector(".js-value");
+let amountElement = document.querySelector(".js-amount");
+let currencyElement = document.querySelector(".js-currency");
 let formElement = document.querySelector(".js-form");
 let resultElement = document.querySelector(".js-result");
 
@@ -7,23 +7,24 @@ let rateEUR = 4.3504;
 let rateGBP = 5.3504;
 
 formElement.addEventListener("submit", (event) => {
-    event.preventDefault();
-    console.log('Formularz został wysłany');
+  event.preventDefault();
+  console.log("Formularz został wysłany");
 
-    let amount = +amountElement.value;
-    let currency = currencyElement.value;
-    let result;
+  let amount = +amountElement.value;
+  let currency = currencyElement.value;
+  let result;
 
-    switch (currency) {
-        case "EUR":
-            result = currency / rateEUR;
-            break;
+  switch (currency) {
+    case "EUR":
+      result = currency / rateEUR;
+      break;
 
-        case "GBP":
-            result = currency / rateGBR;
-            break;
-    }
+    case "GBP":
+      result = currency / rateGBR;
+      break;
+  }
 
-    resultElement.innerHTML = `${amount.toFixed(2)} PLN = <strong>${result.toFixed(2)} ${currency}</strong>`;
-
+  resultElement.innerHTML = `${amount.toFixed(
+    2
+  )} PLN = <strong>${result.toFixed(2)} ${currency}</strong>`;
 });
